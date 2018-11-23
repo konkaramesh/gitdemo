9fd301b30da26e9d8cd1bf677717bd17f2a41c79
devrameshtechnology@gmail.com <devrameshtechnology@gmail.com>
2018-11-23 17:03:08 +0530

    add to git repo

---
+++ src/main/java/com/rest/Github/App.java
@@ -0,0 +1,19 @@
+package com.rest.Github;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public  App() {
+		System.out.println("Hi This is Github Sample");
+		
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
